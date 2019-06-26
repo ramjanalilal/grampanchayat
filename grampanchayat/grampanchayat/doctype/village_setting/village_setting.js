d2677c6f6265b83cd93e55320f54af37287942d6
RS <ramjan@finforce.in>
2019-06-27 01:07:37 +0530

    Village Setting

---
+++ grampanchayat/grampanchayat/doctype/village_setting/village_setting.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, FinForce Consulting LLP and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Village Setting', {
+	// refresh: function(frm) {
+
+	// }
+});
